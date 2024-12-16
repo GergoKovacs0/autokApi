db18931951960454485ee44686ab1917d344581e
GergoKovacs0 <c9kovger@vasvari.org>
2024-12-16 08:22:43 +0100

    added base for controllers

---
+++ src/controllers/cars.js
@@ -0,0 +1,9 @@
+export const getCar = async (req, res, next) => {};
+
+export const getCars = async (req, res, next) => {};
+
+export const createCar = async (req, res, next) => {};
+
+export const updateCar = async (req, res, next) => {};
+
+export const deleteCar = async (req, res, next) => {};
